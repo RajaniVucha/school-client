cf227a25735e6c0884979ef580388ee6b04e4fbf
RajaniVucha <rajani.vucha@gmail.com>
2023-10-20 12:33:07 +0530

    client

---
+++ src/components/EditTeacher.jsx
@@ -0,0 +1,193 @@
+import React, { useState, useEffect, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import SchoolContext from "./SchoolContext";
+
+const EditTeacher = (props) => {
+  const { teacherId } = useParams();
+  //console.log(teacherId);
+  const [newTeacher, setNewTeacher] = useState({});
+  const { getTeacherById, teacher, setTeacher } = useContext(SchoolContext);
+  //console.log(teacher);
+  const navigate = useNavigate();
+
+  const { setError } = useContext(SchoolContext);
+
+  useEffect(() => {
+    //get Teachers by grade
+    getTeacherById(teacherId);
+    //setTeacher(teacherData);
+    //get teachers by grade
+  });
+  const handleChange = (e) => {
+    // alert(e.target.value);
+    const { name, value } = e.target;
+    setTeacher({
+      ...newTeacher,
+      [name]: value,
+    });
+    //console.log(teacher);
+  };
+  const validateForm = () => {
+    var name = document.forms["teacherForm"]["name"].value;
+    var fatherName = document.forms["teacherForm"]["fatherName"].value;
+    var phone = document.forms["teacherForm"]["phone"].value;
+    var email = document.forms["teacherForm"]["email"].value;
+    var address = document.forms["teacherForm"]["address"].value;
+
+    // Name validation (only alphabetic characters and spaces allowed)
+    var namePattern = /^[A-Za-z\s]+$/;
+    if (!name.match(namePattern)) {
+      alert("Invalid name");
+      return false;
+    }
+    if (!fatherName.match(namePattern)) {
+      alert(
+        "Invalid Father Name. Please use only alphabetic characters and spaces."
+      );
+      return false;
+    }
+
+    // Phone number validation (simple format XXX-XXX-XXXX)
+    var phonePattern = /^\d{3}-\d{3}-\d{4}$/;
+    if (!phone.match(phonePattern)) {
+      alert("Invalid phone number. Please use the format XXX-XXX-XXXX.");
+      return false;
+    }
+
+    // Email validation (basic format checking)
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email.match(emailPattern)) {
+      alert("Invalid email address.");
+      return false;
+    }
+
+    // Address validation (any non-empty string is considered valid)
+    if (address.trim() === "") {
+      alert("Please enter a valid address.");
+      return false;
+    }
+
+    // All validations passed
+    return true;
+  };
+
+  //console.log(teacherData);
+  const handleEdit = async (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/teachers/${teacherId}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(teacher),
+          }
+        );
+
+        if (response.ok) {
+          if (response === null) {
+            console.log("Could not find document");
+            setError("Could not find document");
+          } else {
+            // Handle a successful response, e.g., display a success message
+            console.log("Data updated successfully");
+            setError("Teacher updated successfully");
+          }
+          // setEditPage("list");
+        } else {
+          // Handle errors, e.g., display an error message
+          console.error("Error sending data");
+          setError("Error sending data");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        setError(error);
+      }
+      navigate("/teachers");
+    }
+  };
+
+  return (
+    <div className="student-form">
+      <h2>Edit Teacher</h2>
+      <form name="teacherForm">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            defaultValue={teacher[0]?.name}
+            onChange={handleChange}
+            className="input-field"
+          />
+        </div>
+        <div>
+          <label htmlFor="fatherName">Father Name:</label>
+          <input
+            type="text"
+            id="fatherName"
+            name="fatherName"
+            defaultValue={teacher[0]?.fatherName}
+            onChange={handleChange}
+            className="input-field"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          <input
+            type="phone"
+            id="phone"
+            name="phone"
+            defaultValue={teacher[0]?.phone}
+            onChange={handleChange}
+            className="input-field"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            defaultValue={teacher[0]?.email}
+            onChange={handleChange}
+            className="input-field"
+          />
+        </div>
+        <div>
+          <label htmlFor="dateOfBirth">Date Of Birth:</label>
+          <input
+            type="date"
+            id="dateOfBirth"
+            name="dateOfBirth"
+            defaultValue={teacher[0]?.dateOfBirth}
+            onChange={handleChange}
+            className="input-field"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="address">Address:</label>
+          <input
+            type="address"
+            id="address"
+            name="address"
+            defaultValue={teacher[0]?.address}
+            onChange={handleChange}
+            className="input-field"
+          />
+        </div>
+        <button type="submit" className="submit-button" onClick={handleEdit}>
+          Edit Teacher
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default EditTeacher;
---
+++ src/components/MarkList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const markList = () => {
+  return <div>This is lists of marks classwise</div>;
+};
+
+export default markList;
